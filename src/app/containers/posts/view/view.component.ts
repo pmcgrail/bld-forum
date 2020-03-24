28d09151366a591b5e03731719482ad0c20aba7b
mcgrailp <mcgrail.p@gmail.com>
2020-03-24 00:35:30 -0400

    Add user to view post

--- src/app/containers/posts/view/view.component.ts
+++ src/app/containers/posts/view/view.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { PostsService } from 'src/app/providers/posts.service';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+
+import { PostsService, UsersService } from '../../../providers';
+import { IUser, IPostData } from '../../../models';
 
 @Component({
   selector: 'app-view',
@@ -8,13 +12,24 @@ import { PostsService } from 'src/app/providers/posts.service';
   styleUrls: ['./view.component.scss'],
 })
 export class ViewComponent implements OnInit {
-  post$;
+  post$: Observable<IPostData>;
+  user$: Observable<IUser>;
   postId: string;
-  constructor(private route: ActivatedRoute, private service: PostsService) {
-    this.postId = route.snapshot.params['postId'];
+  constructor(
+    private route: ActivatedRoute,
+    private service: PostsService,
+    private usersService: UsersService
+  ) {
+    this.postId = this.route.snapshot.params['postId'];
   }
 
   ngOnInit() {
     this.post$ = this.service.getPost(this.postId);
+    this.user$ = this.post$.pipe(
+      switchMap((post: IPostData) => {
+        return this.usersService.getUser(post.userId);
+      }),
+      map((user: IUser) => user)
+    );
   }
 }
