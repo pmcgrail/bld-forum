1f0f8348e50bfd42ba4a78d8375cf4ddff8dc9a1
mcgrailp <mcgrail.p@gmail.com>
2020-03-24 20:56:52 -0400

    Update new post

--- src/app/containers/posts/new/new.component.ts
+++ src/app/containers/posts/new/new.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 import { PostService } from '../../../providers/post.service';
 import { IUser } from '../../../models/user.interface';
 import { AuthService } from '../../../providers/auth.service';
+import { IPostData } from '../../../models';
 
 @Component({
   selector: 'app-new',
@@ -22,7 +24,11 @@ export class NewComponent implements OnInit, OnDestroy {
   userId: string;
   destroy$: Subject<null> = new Subject();
 
-  constructor(private auth: AuthService, private service: PostService) {
+  constructor(
+    private router: Router,
+    private auth: AuthService,
+    private service: PostService
+  ) {
     this.auth.user$.pipe(takeUntil(this.destroy$)).subscribe((user: IUser) => {
       this.userName = user.displayName;
       this.userId = user.uid;
@@ -30,12 +36,14 @@ export class NewComponent implements OnInit, OnDestroy {
   }
 
   createPost() {
-    const data = {
+    const data: IPostData = {
       ...this.form.value,
-      date: new Date(),
       userId: this.userId,
+      createdDate: new Date(),
     };
-    this.service.createPost(data);
+    this.service.createPost(data).then(() => {
+      this.router.navigate(['/posts']);
+    });
   }
 
   ngOnInit() {}
