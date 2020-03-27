76d972ac9c0de4de4e972abf32720c3f523709b8
mcgrailp <mcgrail.p@gmail.com>
2020-03-26 20:44:47 -0400

    Update home page

--- src/app/containers/home/home.component.ts
+++ src/app/containers/home/home.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from 'src/app/providers';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  authUser$;
 
-  ngOnInit(): void {}
+  constructor(public auth: AuthService) {
+    this.authUser$ = this.auth.user$;
+  }
+
+  loginFacebook() {
+    this.auth.facebookSignin();
+  }
+
+  ngOnInit() {}
 }
