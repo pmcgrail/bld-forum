530d10314314491e04a4c537a2932a6c4a539e98
Patrick McGrail <patrick.mcgrail@readyeducation.com>
2020-04-05 23:48:30 -0400

    Back button component

---
+++ src/app/components/back-button/back-button.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-back-button',
+  templateUrl: './back-button.component.html',
+  styleUrls: ['./back-button.component.scss'],
+})
+export class BackButtonComponent implements OnInit {
+  @Input() link: string;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
