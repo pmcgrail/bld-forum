1bbd85d2060f580c7ab54404d2aac955df676dd1
mcgrailp <mcgrail.p@gmail.com>
2020-04-08 16:19:36 -0400

    Update comment max size

--- src/app/data/constants.ts
+++ src/app/data/constants.ts
@@ -3,9 +3,9 @@ export const MAX_COMMENTS = 10;
 export const MAX_DATE = new Date(8640000000000);
 
 export const POST_TITLE_MIN = 5;
-export const POST_TITLE_MAX = 50;
+export const POST_TITLE_MAX = 100;
 export const POST_TEXT_MIN = 10;
 export const POST_TEXT_MAX = 5000;
 
 export const COMMENT_MIN = 5;
-export const COMMENT_MAX = 500;
+export const COMMENT_MAX = 5000;
