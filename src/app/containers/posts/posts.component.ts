0ff032ceb5d9daac841e6cbdddb123b2885361ce
mcgrailp <mcgrail.p@gmail.com>
2020-04-07 22:08:49 -0400

    Display categories error

--- src/app/containers/posts/posts.component.ts
+++ src/app/containers/posts/posts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 import { ICategory } from '../../models';
 import { CategoryService } from 'src/app/providers/category.service';
@@ -10,11 +11,20 @@ import { CategoryService } from 'src/app/providers/category.service';
   styleUrls: ['./posts.component.scss'],
 })
 export class PostsComponent implements OnInit {
+  categoriesError = false;
   categories$: Observable<ICategory[]>;
 
   constructor(private service: CategoryService) {}
 
+  onCategoriesError = (error) => {
+    console.error(error);
+    this.categoriesError = true;
+    return of(undefined);
+  };
+
   ngOnInit() {
-    this.categories$ = this.service.categories$;
+    this.categories$ = this.service.categories$.pipe(
+      catchError(this.onCategoriesError)
+    );
   }
 }
